4d882b254bb389b85af0f5006d03bfc920ede33c
Christopher Schreiner <cs@elunic.com>
2021-07-27 12:26:07 +0200

    chore: update and add deploy action

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=4.3.2
+VERSION=4.4
 
 sed -i 's/main/main non-free/' /etc/apt/sources.list
 
@@ -34,3 +34,6 @@ cd ..
 rm -rf ffmpeg-$VERSION*
 
 pip3 install --upgrade youtube_dl
+
+apt-get -y purge gcc bzip2 nasm yasm autoconf automake build-essential cmake git-core libass-dev libfreetype6-dev libsdl2-dev libtool libva-dev libvdpau-dev libvorbis-dev libxcb1-dev libxcb-shm0-dev libxcb-xfixes0-dev pkg-config texinfo wget zlib1g-dev libx265-dev libnuma-dev libfdk-aac-dev libmp3lame-dev libopus-dev libx264-dev libssl-dev
+apt-get clean

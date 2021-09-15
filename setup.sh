e7887c44dde9858dacdcd4670a44c8faf9f1d3dc
Christopher Schreiner <cs@elunic.com>
2021-09-15 17:43:54 +0200

    fix: cannot write cache

--- setup.sh
+++ setup.sh
@@ -37,3 +37,6 @@ pip3 install --upgrade youtube_dl
 
 apt-get -y purge gcc bzip2 nasm yasm autoconf automake build-essential cmake git-core libass-dev libfreetype6-dev libsdl2-dev libtool libva-dev libvdpau-dev libvorbis-dev libxcb1-dev libxcb-shm0-dev libxcb-xfixes0-dev pkg-config texinfo wget zlib1g-dev libx265-dev libnuma-dev libfdk-aac-dev libmp3lame-dev libopus-dev libx264-dev libssl-dev
 apt-get clean
+
+mkdir -p /.cache
+chmod 777 /.cache

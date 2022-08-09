0ce6377766f4a90fb98ff936193351755544ee40
Christopher Schreiner <git@infanf.com>
2022-08-09 23:39:13 +0200

    feat: add yt-dlp

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=5.0.1
+VERSION=5.1
 
 sed -i 's/main/main non-free/' /etc/apt/sources.list
 
@@ -33,7 +33,7 @@ hash -r
 cd ..
 rm -rf ffmpeg-$VERSION*
 
-pip3 install --upgrade youtube_dl
+pip3 install --upgrade youtube_dl yt-dlp
 
 apt-get -y purge gcc bzip2 nasm yasm autoconf automake build-essential cmake git-core libass-dev libfreetype6-dev libsdl2-dev libtool libva-dev libvdpau-dev libvorbis-dev libxcb1-dev libxcb-shm0-dev libxcb-xfixes0-dev pkg-config texinfo wget zlib1g-dev libx265-dev libnuma-dev libfdk-aac-dev libmp3lame-dev libopus-dev libx264-dev libssl-dev
 apt-get clean
